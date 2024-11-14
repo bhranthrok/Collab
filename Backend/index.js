b836795fd2be5660b91f75d4b6e675da3dbc1f57
bhranthrok <luizcauetcastilla@gmail.com>
2024-11-14 14:21:03 -0600

    Bcrypt Fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 // Node.js Server Setup
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require("multer");
 const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3");
 require('dotenv').config({path: './info.env'});
