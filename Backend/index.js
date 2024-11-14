437489db34e3b24a2483367a9bd441f1eb5b9d2c
bhranthrok <luizcauetcastilla@gmail.com>
2024-11-13 20:09:29 -0600

    Updated to Bcryptjs

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 // Node.js Server Setup
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 const PORT = 4000;
